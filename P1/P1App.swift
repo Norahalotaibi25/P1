8d094a9041a6c08dc40012463b77c49523317a06
Norah <nalotaibia25@twq.idserve.net>
2025-10-19 10:17:27 +0300

    Initial Commit

---
+++ P1/P1App.swift
@@ -0,0 +1,17 @@
+//
+//  P1App.swift
+//  P1
+//
+//  Created by Norah Abdulkairm on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct P1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ P1Tests/P1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  P1Tests.swift
+//  P1Tests
+//
+//  Created by Norah Abdulkairm on 27/04/1447 AH.
+//
+
+import Testing
+@testable import P1
+
+struct P1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
